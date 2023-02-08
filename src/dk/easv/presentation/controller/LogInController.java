8b5bc8e6f082b5c7ebd240c8fa9f03b24ecb76e1
JennyTromTheBlock <kenmad01@easv365.dk>
2023-02-08 20:00:19 +0100

    finish animation

--- src/dk/easv/presentation/controller/LogInController.java
+++ src/dk/easv/presentation/controller/LogInController.java
@@ -2,6 +2,7 @@ package dk.easv.presentation.controller;
 
 import dk.easv.entities.User;
 import dk.easv.presentation.model.AppModel;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,10 +15,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.io.IOException;
@@ -30,16 +33,22 @@ public class LogInController implements Initializable {
     public GridPane loginInputGridPane;
     public GridPane loginInputGridPane1;
     public ImageView loginLogo;
+    public Label goToCreateAccount;
+    public Label goToLogInForm;
+    public TextField userId1;
+    public PasswordField passwordField1;
     @FXML private PasswordField passwordField;
     @FXML private TextField userId;
     private AppModel model;
 
+    private boolean isLoginSliderLeft = true;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         Image img = new Image("Tv-Remote-50-Red.png");
         loginLogo.setImage(img);
         model = new AppModel();
+
     }
 
     public void logIn(ActionEvent actionEvent) {
@@ -73,6 +82,51 @@ public class LogInController implements Initializable {
 
     public void signUp(ActionEvent actionEvent) {
         System.out.println("Sign-Up");
+    }
+
+    public void handleSliderOnPress(MouseEvent mouseEvent) {
+        int direction = 350;
+        if(isLoginSliderLeft){
+            direction = 350;
+            isLoginSliderLeft = false;
+
+        }else {
+            direction= -350;
+            isLoginSliderLeft = true;
+        }
+        toogleSignInForm();
+
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(loginSlider);
+        translate.setDuration(Duration.millis(500));
+        translate.setByX(direction);
+        translate.play();
+
+    }
+
+    /**
+     * sets the buttons and inoutFileds active or disables them if createAccount is selected or not
+     */
+    private void toogleSignInForm() {
+        if (isLoginSliderLeft) {
+            //sets sign in form false
+            goToCreateAccount.setDisable(false);
+            userId.setDisable(false);
+            passwordField.setDisable(false);
+            //sets log in true
+            goToLogInForm.setDisable(true);
+            userId1.setDisable(true);
+            passwordField1.setDisable(true);
+
+        }else {
+            goToCreateAccount.setDisable(true);
+            userId.setDisable(true);
+            passwordField.setDisable(true);
+
+            goToLogInForm.setDisable(false);
+            userId1.setDisable(false);
+            passwordField1.setDisable(false);
+        }
     }
 
 }
