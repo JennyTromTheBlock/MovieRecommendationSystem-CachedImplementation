1d2dd42b02c1355883bc3cf48efcfb198f3bf778
JennyTromTheBlock <kenmad01@easv365.dk>
2023-02-14 19:13:25 +0100

    j,

--- src/dk/easv/presentation/controller/MainController.java
+++ src/dk/easv/presentation/controller/MainController.java
@@ -1,22 +1,43 @@
 package dk.easv.presentation.controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     public ImageView imgViewLogo;
+    public VBox mainViewSidebar;
+    public FlowPane flow;
+    public BorderPane borderPane;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Image img = new Image("Tv-Remote-50-Red.png");
-        imgViewLogo.setImage(img);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dk/easv/presentation/view/MovieCard.fxml"));
+        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/dk/easv/presentation/view/HomeView.fxml"));
+        try {
+            VBox v = loader.load();
+            AnchorPane v1 = loader1.load();
+            borderPane.setCenter(v1);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void handleHome(ActionEvent actionEvent) {
